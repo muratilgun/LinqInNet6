2b8c813d910461a6336e01b3a143a300bc445fa8
muratilgun <muratilgun34@gmail.com>
2021-10-29 00:43:10 +0300

    TakeRange

---
+++ TakeRange/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+var names = new[] {"Nick", "Mike","John","Leyla","David","Damian"};
+
+var slice = names.Skip(2).Take(2);
+// "John" , "Leyla"
+var slicee = names.Take(2..4);
+// "John" , "Leyla"
+
+var sliceee = names.Take(^4..4);
+// "John" , "Leyla"
+// .Net 6 ile Take metotuna aralık ekleyebilme ve index operatörünü kabul etme özellikleri eklendi
+Console.ReadKey();
