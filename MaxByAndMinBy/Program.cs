41d88dd593c8027a205c7de6b9f7f1358739973d
muratilgun <muratilgun34@gmail.com>
2021-10-28 23:54:54 +0300

    MaxByAndMinBy

---
+++ MaxByAndMinBy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+var students = new[]
+{
+    new Student("Niyazi Bakkal", 28),
+    new Student("Recep Ivedik", 22),
+    new Student("Hatice Kaynak", 24)
+};
+var youngest = students.MinBy(x => x.Age);
+var oldest = students.MaxBy(x => x.Age);
+
+#region old
+//
+// var youngest = students.OrderBy(x => x.Age).First();
+// var oldest = students.OrderByDescending(x => x.Age).First();
+
+#endregion
+Console.ReadKey();
+public class Student
+{
+    public Student(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
